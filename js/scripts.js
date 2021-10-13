9dd95fdce333c4236ccfca814b97ed36564a4ab7
vacationstays <vacationstays.club@gmail.com>
2021-10-12 23:05:31 -0500

    update

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,20 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+
+var acc = document.getElementsByClassName('accordion');
+var i ;
+var len = acc.length;
+for (i=0;i<len;i++){
+    acc[i].addEventListener('click', function(){
+        this.classList.toggle('faq_active');
+        var panel = this.nextElementSibling;
+        if(panel.style.maxHeight){
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + 'px'
+        }
+    })
+}
